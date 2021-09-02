ca90a99e87b69a25e2b1fa4a844a05588f91ce65
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-09-02 22:09:44 +0200

    print 3,2,1

--- mocking/my/countdown.go
+++ mocking/my/countdown.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 func Countdown(writer io.Writer) {
-	fmt.Fprint(writer, "3")
+	for i := 3; i > 0; i-- {
+		fmt.Fprintf(writer, "%d\n", i)
+	}
+	fmt.Fprint(writer, "Go!")
+}
+
+func main() {
+	Countdown(os.Stdout)
 }
--- mocking/my/countdown_test.go
+++ mocking/my/countdown_test.go
@@ -11,5 +11,9 @@ func TestCountdown(t *testing.T) {
 	// print 3, then 1s later 2, then 1 and then Go!
 	buffer := &bytes.Buffer{}
 	Countdown(buffer)
-	assert.Equal(t, "3", buffer.String())
+	want := `3
+2
+1
+Go!`
+	assert.Equal(t, want, buffer.String())
 }
