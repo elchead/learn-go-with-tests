e7c63c2ad7d85ba31d06af096e9e3dde7c26b294
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-09-04 17:43:57 +0200

    pass arabic conversion till 5

--- roman-numerals/my/roman.go
+++ roman-numerals/my/roman.go
@@ -8,10 +8,40 @@ type RomanLiterals struct {
 	Value  int
 	Symbol string
 }
+type allRomanNumerals []RomanLiterals
 
+func (pairs allRomanNumerals) ValueOf(roman string) int {
+	for _, pair := range pairs {
+		if roman == pair.Symbol {
+			return pair.Value
+		}
+	}
+	return 0
+}
+
+var allLiterals = allRomanNumerals{{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}}
+
+func ConvertToArabic(roman string) int {
+	var result int
+	for i := 0; i < len(roman); i++ {
+		if i+1 < len(roman) {
+			nextSymbol := roman[i+1]
+			potentialNumber := string([]byte{roman[i], nextSymbol})
+			value := allLiterals.ValueOf(potentialNumber)
+			if value != 0 {
+				i++
+				result += value
+			} else {
+				result++
+			}
+		} else {
+			result++
+		}
+	}
+	return result
+}
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
-	allLiterals := []RomanLiterals{{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}}
 	for _, numeral := range allLiterals {
 		for arabic >= numeral.Value {
 			result.WriteString(numeral.Symbol)
--- roman-numerals/my/roman_test.go
+++ roman-numerals/my/roman_test.go
@@ -6,25 +6,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
-		Arabic int
-		Want   string
-	}{
-		{3, "III"},
-		{4, "IV"},
-		{5, "V"},
-		{7, "VII"},
-		{9, "IX"},
-		{11, "XI"},
-		{39, "XXXIX"},
-		{56, "LVI"},
-		{1984, "MCMLXXXIV"},
-	}
+var cases = []struct {
+	Arabic int
+	Roman  string
+}{
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{7, "VII"},
+	{9, "IX"},
+	{11, "XI"},
+	{39, "XXXIX"},
+	{56, "LVI"},
+	{1984, "MCMLXXXIV"},
+}
+
+func TestAllValueOf(t *testing.T) {
+	res := allLiterals.ValueOf("X")
+	assert.Equal(t, 10, res)
+}
+func TestConvertingToRoman(t *testing.T) {
 	for _, c := range cases {
 		t.Run("convert", func(t *testing.T) {
 			res := ConvertToRoman(c.Arabic)
-			assert.Equal(t, c.Want, res)
+			assert.Equal(t, c.Roman, res)
+		})
+	}
+}
+
+func TestConvertingToArabic(t *testing.T) {
+	for _, c := range cases[:2] {
+		t.Run("convert", func(t *testing.T) {
+			res := ConvertToArabic(c.Roman)
+			assert.Equal(t, c.Arabic, res)
 		})
 	}
 }
