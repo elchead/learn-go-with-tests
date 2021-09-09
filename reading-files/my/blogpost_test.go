a7ec3817221fe05f000f1d382f55679ebf866d7b
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-09-09 16:32:52 +0200

    assert fail open error

--- reading-files/my/blogpost_test.go
+++ reading-files/my/blogpost_test.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 
 	"github.com/stretchr/testify/assert"
 )
+
+type StubFailingFS struct {
+}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("oh no, i always fail")
+}
 
 func TestPostFromFolder(t *testing.T) {
 	fs := fstest.MapFS{
@@ -15,4 +24,9 @@ func TestPostFromFolder(t *testing.T) {
 	posts, err := NewPostsFromFS(fs)
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(posts))
+}
+
+func TestErrorOpen(t *testing.T) {
+	_, err := NewPostsFromFS(StubFailingFS{})
+	assert.Error(t, err)
 }
