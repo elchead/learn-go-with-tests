5476389b581e83f269f2862b709cfc54eeb6883a
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-09-09 17:14:55 +0200

    refactor signatures for loose coupling

--- reading-files/my/blockpost.go
+++ reading-files/my/blockpost.go
@@ -9,14 +9,17 @@ type Post struct {
 	Title string
 }
 
-func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
-	file, err := fileSystem.Open(f.Name())
+func getPost(fileSystem fs.FS, filename string) (Post, error) {
+	file, err := fileSystem.Open(filename)
 	if err != nil {
 		return Post{}, err
 	}
 	defer file.Close()
+	return newPost(file)
+}
 
-	data, err := io.ReadAll(file)
+func newPost(f io.Reader) (Post, error) {
+	data, err := io.ReadAll(f)
 	return Post{Title: string(data[7:])}, err
 }
 
@@ -27,7 +30,7 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	}
 	var posts []Post
 	for _, entry := range dir {
-		if post, err := getPost(fileSystem, entry); err == nil {
+		if post, err := getPost(fileSystem, entry.Name()); err == nil {
 			posts = append(posts, post)
 		}
 
