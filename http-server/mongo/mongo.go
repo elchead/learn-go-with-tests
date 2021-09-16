53c959e49ec7d8ab35af4475a223f08459b07560
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-09-16 08:25:04 +0200

    move mongo to new package

---
+++ http-server/mongo/mongo.go
@@ -0,0 +1,11 @@
+package mongo
+
+// type MongoStore struct {
+// }
+
+// func (s *MongoStore) GetPlayerScore(name string) (int, bool) {
+// }
+
+// func (s *MongoStore) PostPlayerWin(name string) error {
+
+// }
