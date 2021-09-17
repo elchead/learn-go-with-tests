fa9d8aac36d54a6d99f99f8df49a94be594628fd
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-09-17 08:25:10 +0200

    refactor File reading

--- http-server/my/filesystem.go
+++ http-server/my/filesystem.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
-	"log"
 )
 
 type FileSystemPlayerStore struct {
-	reader io.Reader
+	database io.Reader
+}
+
+func NewLeague(rdr io.Reader) ([]Player, error) {
+	var got []Player
+	err := json.NewDecoder(rdr).Decode(&got)
+	if err != nil {
+		fmt.Errorf("Could not parse league, %v", err)
+	}
+	return got, err
 }
 
 func (s FileSystemPlayerStore) GetLeague() []Player {
-	var got []Player
-	err := json.NewDecoder(s.reader).Decode(&got)
-	if err != nil {
-		log.Fatal("Could not decode database")
-		return nil
-	}
-	return got
+	league, _ := NewLeague(s.database)
+	return league
 }
