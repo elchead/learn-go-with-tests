2aeab9250029bf294d4ad6dfe89da1bfc985b9f1
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-09-18 13:31:23 +0200

    extract winner from input

--- command-line/my/cli.go
+++ command-line/my/cli.go
@@ -1,9 +1,22 @@
 package poker
 
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
 type CLI struct {
 	store PlayerStore
+	input io.Reader
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
 }
 
 func (c *CLI) PlayPoker() {
-	c.store.RecordWin("Hi")
+	reader := bufio.NewScanner(c.input)
+	reader.Scan()
+	c.store.RecordWin(extractWinner(reader.Text()))
 }
--- command-line/my/cli_test.go
+++ command-line/my/cli_test.go
@@ -1,15 +1,18 @@
 package poker
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader("Chris wins\n")
 	store := &StubPlayerStore{}
-	cli := &CLI{store}
+	cli := &CLI{store, in}
 	cli.PlayPoker()
 	assert.Equal(t, 1, len(store.winCalls))
+	assert.Equal(t, "Chris", store.winCalls[0])
 
 }
